f3bc189530c2e37919e3a7932da416ef6d758dce
Mahshid <mahshojaei2001@gmail.com>
2025-05-11 16:50:35 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import  React from 'react';
+import Quiz from "./Component/Quix.jsx";
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+
+    }
+
+    render() {
+        return (
+            <Quiz/>
+        )
+    }
+}
